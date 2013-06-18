d78a60e53b5ffbeba273b81f1ab1745403347d23
ctstone <ctstone@gmail.com>
2013-06-17 23:32:09 -0400

    added responsive css and social links

--- EliseCouture.Web/Controllers/BlogController.cs
+++ EliseCouture.Web/Controllers/BlogController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ctstone.Tumblr;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,13 +9,18 @@ namespace EliseCouture.Web.Controllers
 {
     public class BlogController : Controller
     {
-        //
-        // GET: /Blog/
+        const string ClientKey = "cTLPNguFD505HDWDcusDq3KcCjAaF2fr02fgvJu8f5kNx1Ip2r";
+        const string ClientSecret = "TA0WeGbvQSLu3n84jjKHZh4g7QoMcTLmjrZ6GjvWwbBdFoeKFZ";
 
-        public ActionResult Index()
+        public ActionResult Index(string x, string y)
         {
-            return View();
-        }
+            string authorizedToken = "Hp6hOEFek6rujYpVGChGo3mOBY72sARiy8zLCTCHgTyuGvP88C";
+            string authorizedTokenSecret = "yaUwgDzoJFUh7bGp2jnAhvp1MgZekN8bCMBnPvBfWDlhIYy1uX";
+            string blog = "pedalboston.tumblr.com";
 
+            TumblrClient tumblr = new TumblrClient(ClientKey, ClientSecret, String.Empty, authorizedToken, authorizedTokenSecret);
+
+            return View(tumblr.Blog.GetPosts(blog).response.posts);
+        }
     }
 }
